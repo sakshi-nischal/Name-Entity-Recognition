517da06c28550340829c1714a9fb51e7b7469b16
Sakshi Nischal <57134054+sakshi-nischal@users.noreply.github.com>
2021-04-12 12:45:11 +0530

    Update ner.py

--- ner.py
+++ ner.py
@@ -1,3 +1,4 @@
+#IMPORT IMPORTANT LIBRARIES
 import streamlit as st
 import wikipedia
 import spacy
@@ -5,19 +6,25 @@ from spacy import displacy
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+#ENGLISH PIPELINE OPTIMIZED FOR CPU
+#COMPONENTS: tok2vec, tagger, parser, senter, ner, attribute_ruler, lemmatizer
 nlp = spacy.load("en_core_web_sm")
+
+#HTML STYLING
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 0 solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
 
+#TO EXTRACT CONTENT FROM INPUT URL
 @st.cache
 def text_url(url):
     page=urlopen(url)
     soup=BeautifulSoup(page)
     text_extracted=' '.join(map(lambda p:p.text,soup.find_all('p')))
     return text_extracted
-
+#MAIN FUNCTION
 def main():
     st.title("Named Entity Recognition")
     operations=["Name Enitity Recognition: Summary","Name Enitity Recognition: URL"]
+    #SIDEBAR FOR SELECTING BETWEEN TWO OPERATIONS
     choice=st.sidebar.selectbox("Select Operation",operations)
 
     if choice=="Name Enitity Recognition: Summary":
